4243b59a8c887a32281e2482aeae01881c8c1749
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2022-09-11 16:09:37 -0300

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-n1 = float(input ('Digite um valor: '))
-s = n1
-g = n1 * 10
-m = n1 * 100
-d = n1 * 1000
-h = n1 / 100
-q = n1 / 10
-y = n1 / 1000
-print ('{}m {}dm {}cm {}mm {}hm {}dam {}km'.format(s, g, m, d, h, q, y))
